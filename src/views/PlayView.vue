80dad32a5e3e28b1bef49019e292ac108b084bdf
Orange <2497436726@qq.com>
2022-10-26 23:23:40 +0800

    :sparkles: active the ball

--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -1,27 +1,42 @@
 <template>
   <div class="play">
     <div id="playground">
-      <span id="ball"></span>
+      <span id="ball" ref="ball"></span>
     </div>
   </div>
 </template>
 <script setup>
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
+const ball = ref(null);
 onMounted(() => {
-  console.log(1);
+  let speedX = parseInt(Math.random() * 4) + 3;
+  let speedY = parseInt(Math.random() * 5) + 3;
+  const oball = ball.value;
+  const tick = () => {
+    oball.style.left = oball.offsetLeft + speedX + 'px';
+    oball.style.top = oball.offsetTop + speedY + 'px';
+    if (oball.offsetLeft >= 980 || oball.offsetLeft <= 0) {
+      speedX *= -1;
+    }
+    if (oball.offsetTop >= 480 || oball.offsetTop <= 0) {
+      speedY *= -1;
+    }
+    requestAnimationFrame(tick);
+  };
+  tick();
 });
 </script>
 <style scoped>
 @media (min-width: 1024px) {
   .play {
-    min-height: 100vh;
+    min-height: 90vh;
     display: flex;
     align-items: center;
   }
 }
 #playground {
-  width: 80vw;
-  height: 80vh;
+  width: 1000px;
+  height: 500px;
   border: solid 2px red;
   border-radius: 8px;
   position: relative;
